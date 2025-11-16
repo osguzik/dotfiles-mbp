a3768ea2fc6dee2d1765baa29eedaaf90b6de158
oskarggu <os.guzik@proton.me>
2025-11-16 02:30:28 +0100

    Initial commit

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Disable scrolling animations
+vim.g.snacks_animate = false
+
+-- Indentation
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+
+vim.g.autoformat = false
